c3d9ad7494c9a3e8901e8aa5325d113544e8448a
nzurita <info@nzurita.es>
2026-09-01 20:15:37 +0200

    Barra de status como componente aparte

---
+++ frontend/src/app/main/ui/app_status.cljs
@@ -0,0 +1,25 @@
+(ns app.main.ui.app-status
+  (:require
+   [rumext.v2 :as mf]))
+
+(def app-status-size 10)
+
+(defn push-status!
+  [app-status entry]
+  (swap! app-status (fn [xs] (take app-status-size (conj xs entry)))))
+
+(mf/defc app-status*
+  {::mf/props :obj}
+  [{:keys [app-status]}]
+  (let [app-status-expanded (mf/use-state false)]
+    [:footer.status-bar
+              {:class (when @app-status-expanded "expanded")}
+            [:button {:class "btn-expand-collapse" :on-click #(swap! app-status-expanded not)}]
+           [:ul.status-list
+                (for [[idx msg] (map-indexed vector app-status)]
+                  (let [type (name (:type msg))]
+                  [:li {:key idx :class type}
+                   [:span (:msg msg)]]))
+                ]
+         ]
+  ))
--- frontend/src/app/main/ui/layout.cljs
+++ frontend/src/app/main/ui/layout.cljs
@@ -1,16 +1,11 @@
 (ns app.main.ui.layout
   (:require
    [app.main.api :as api]
+   [app.main.ui.app-status :as status] ; :refer [app-status*]
    [app.main.ui.character-list :refer [character-list*]]
    [app.main.ui.character-card :refer [character-card*]]
    [app.main.ui.planet-card :refer [planet-card*]]
    [rumext.v2 :as mf]))
-
-(def app-status-size 10)
-
-(defn- push-status!
-  [app-status entry]
-  (swap! app-status (fn [xs] (take app-status-size (conj xs entry)))))
 
 (mf/defc layout*
   []
@@ -20,13 +15,12 @@
         planet                (mf/use-state nil)
         loading?              (mf/use-state false)
         loading-planet?       (mf/use-state false)
-        app-status            (mf/use-state (list {:type :info :msg "Ready"}))
-        app-status-expanded   (mf/use-state false)]
+        app-status            (mf/use-state (list {:type :info :msg "Ready"}))]
 
     (mf/with-effect [selected-character-id]
       (when @selected-character-id
         (reset! loading? true)
-        (push-status! app-status {:type :info :msg "Cargando"})
+        (status/push-status! app-status {:type :info :msg "Cargando"})
 
         (-> (api/get-character @selected-character-id)
             (.then (fn [^js data]
@@ -34,29 +28,29 @@
                      (if (.-ok data)
                        (do
                            (reset! character (.-character data))
-                              (push-status! app-status {:type :info :msg (str "Cargado personaje " (.-id (.-character data)))})
+                              (status/push-status! app-status {:type :info :msg (str "Cargado personaje " (.-id (.-character data)))})
 
                            (reset! selected-planet-id (.-id (.-originPlanet (.-character data))) ))
-                       (push-status! app-status {:type :error :msg (.-error data)}) )))
+                       (status/push-status! app-status {:type :error :msg (.-error data)}) )))
             (.catch (fn [err]
                       (reset! loading? false)
-                       (push-status! app-status {:type :error :msg (str err)}) )))))
+                       (status/push-status! app-status {:type :error :msg (str err)}) )))))
 
     (mf/with-effect [selected-planet-id]
       (when @selected-planet-id
         (reset! loading-planet? true)
-        (push-status! app-status {:type :info :msg "Cargando"})
+        (status/push-status! app-status {:type :info :msg "Cargando"})
         (-> (api/get-planet @selected-planet-id)
             (.then (fn [^js data]
                      (reset! loading-planet? false)
                      (if (.-ok data)
                        (do
                            (reset! planet (.-planet data))
-                           (push-status! app-status {:type :info :msg (str "Cargado planeta " (.-id (.-planet data)))}))
-                       (push-status! app-status {:type :error :msg (.-error data)}) )))
+                           (status/push-status! app-status {:type :info :msg (str "Cargado planeta " (.-id (.-planet data)))}))
+                       (status/push-status! app-status {:type :error :msg (.-error data)}) )))
             (.catch (fn [err]
                       (reset! loading-planet? false)
-                       (push-status! app-status {:type :error :msg (str err)}) )))))
+                       (status/push-status! app-status {:type :error :msg (str err)}) )))))
 
 
     [:div.app-shell
@@ -68,16 +62,7 @@
        [:> character-list* {:selected-character-id @selected-character-id
                             :on-select #(reset! selected-character-id %)}]]
 
-       [:footer.status-bar
-            {:class (when @app-status-expanded "expanded")}
-          [:button {:class "btn-expand-collapse" :on-click #(swap! app-status-expanded not)}]
-         [:ul.status-list
-              (for [[idx msg] (map-indexed vector @app-status)]
-                (let [type (name (:type msg))]
-                [:li {:key idx :class type}
-                 [:span (:msg msg)]]))
-              ]
-       ]
+       [:> status/app-status* {:app-status @app-status}]
      ]
 
       [:main.main-panel
